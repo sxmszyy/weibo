55cc247d6dd93c7001d4bb9a90ccba6283753084
zhengyuanyuan <1078547273@qq.com>
2019-04-15 17:39:39 +0800

    修复日期异常bug

--- src/Android端代码/WeiBo/app/src/main/java/com/example/hp/weibo/ZhuanfaActivity.java
+++ src/Android端代码/WeiBo/app/src/main/java/com/example/hp/weibo/ZhuanfaActivity.java
@@ -82,7 +82,7 @@ public class ZhuanfaActivity extends AppCompatActivity {
                 //年
                 int year = calendar.get(Calendar.YEAR);
                 //月
-                int month = calendar.get(Calendar.MONTH);
+                int month = calendar.get(Calendar.MONTH) + 1;
                 //日
                 int day = calendar.get(Calendar.DAY_OF_MONTH);
                 //小时
